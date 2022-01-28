ddfbb1fe630ef422d25c129f0113d3d360c6d6e7
Roomcar <noteforziio@163.com>
2022-01-28 17:28:19 +0800

    save developing

---
+++ js/components/TheSelector.js
@@ -0,0 +1,106 @@
+const TheSelector = (tokenList) => {
+
+
+  const selection = reactive({
+    isSelecting: false,
+    start: null,
+    end: null,
+    end: null,
+    array: [],
+    again: false,
+  });
+
+
+ const selectionMethods = {
+    clearSelection: () => {
+      Object.assign(selection, {
+        isSelecting: false,
+        start: null,
+        end: null,
+        end: null,
+        array: [],
+        again: false,
+      });
+      for (let tkn of tokenList) {
+        if (tkn._ctrl != null) {
+          Object.assign(tkn._ctrl, {
+            selecting: false,
+            selected: false,
+          })
+        };
+      };
+    },
+    onMouseDown:  (token) => {
+      // console.log(['mouseDown', token.word]);
+      //
+      if (selection.isSelecting) {
+        selection.again = true;
+        return;
+      };
+      //
+      selection.again = false;
+      for (let tkn of tokenList) {
+        tkn._ctrl = tkn._ctrl ?? {};
+        tkn._ctrl.selected = false;
+      };
+      token._ctrl.selecting = true;
+      //
+      selection.isSelecting = true;
+      selection.start = token.idx;
+      selection.current = token.idx;
+      selection.end = null;
+    },
+    onMouseEnter: (token) => {
+      // console.log(['mouseEnter', token.word]);
+      if (!selection.isSelecting) { return; };
+      selection.current = token.idx;
+      let [aa, bb] = [selection.start, selection.current];
+      if (bb < aa) {
+        [bb, aa] = [aa, bb];
+      };
+      let array = []
+      for (let idx = aa; idx<=bb; idx++) {
+        array.push(idx);
+        for (let tkn of tokenList) {
+          tkn._ctrl.selecting = (array.includes(tkn.idx));
+        };
+      };
+    },
+    onMouseOut:   (token) => {
+      selection.current = null;
+      // console.log(['mouseOut', token.word]);
+    },
+    onMouseUp:    (token) => {
+      // console.log(['mouseUp', token.word]);
+      //
+      if (selection.start == null) {return;};
+      //
+      if (selection.start == token.idx) {
+        if (!selection.again) {return;};
+        selection.again = false;
+      };
+      //
+      selection.end = token.idx;
+      let [aa, bb] = [selection.start, selection.end];
+      if (bb < aa) {
+        [bb, aa] = [aa, bb];
+      };
+      selection.isSelecting = false;
+      selection.array = [];
+      for (let idx = aa; idx<=bb; idx++) {
+        selection.array.push(idx);
+        for (let tkn of tokenList) {
+          if (selection.array.includes(tkn.idx)) {
+            // tkn._ctrl = tkn._ctrl ?? {};
+            tkn._ctrl.selecting = false;
+            tkn._ctrl.selected = true;
+          }
+        };
+      };
+      // console.log(selection.array);
+    },
+  };
+
+
+  return { selection: toRefs(selection), selectionMethods };
+};
---
+++ js/util.js
@@ -0,0 +1,6 @@
+
+const timeString = () => {
+  let the_date = new Date();
+  let str = `${(''+the_date.getFullYear()).slice(2,4)}${(''+(the_date.getMonth()+1)).length==1?'0':''}${the_date.getMonth()+1}${(''+the_date.getDate()).length==1?'0':''}${the_date.getDate()}-${(''+the_date.getHours()).length==1?'0':''}${the_date.getHours()}${(''+the_date.getMinutes()).length==1?'0':''}${the_date.getMinutes()}${(''+the_date.getSeconds()).length==1?'0':''}${the_date.getSeconds()}`;
+  return str;
+};
